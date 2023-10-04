c56c9d907b28956f41ad23486421d7f1636ba8fd
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-10-04 13:35:55 +0300

    first  app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,16 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:const Scaffold(),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
     );
   }
 }
