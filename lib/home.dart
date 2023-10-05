4e9b05a811001d141f881db164144d3a356dceab
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-10-05 04:48:53 +0300

    flutter cyrrency

--- lib/home.dart
+++ lib/home.dart
@@ -7,8 +7,18 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.blueGrey,
       body: Center(
-        child: Text('Hello Developer!!!'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('0', style: TextStyle(
+              fontSize: 45,
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 255, 255, 255)
+            )),
+          ],
+        ),
       ),
     );
   }
