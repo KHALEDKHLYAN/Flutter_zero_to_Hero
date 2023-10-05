44228992e0b0b6bddb1bb8af6a0396103482e73c
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-10-05 05:28:10 +0300

    flutter cyrrency

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class Home extends StatelessWidget {
   const Home({super.key});
 
@@ -12,11 +11,27 @@ class Home extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('0', style: TextStyle(
-              fontSize: 45,
-              fontWeight: FontWeight.bold,
-              color: Color.fromARGB(255, 255, 255, 255)
-            )),
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            TextField(
+              style: TextStyle(
+                color: Colors.white60,
+              ),
+              decoration: InputDecoration(
+                hintText: 'PLease enter the amount in USD',
+                hintStyle: TextStyle(
+                  color: Colors.white60,
+                ),
+                prefixIcon: Icon(Icons.monetization_on_outlined),
+                prefixIconColor: Colors.white60,
+              ),
+            )
           ],
         ),
       ),
