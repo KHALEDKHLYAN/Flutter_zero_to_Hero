3e71aa13b8909eed4896125681bad958ba4a23ba
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-10-07 05:26:17 +0300

    currency converter

--- lib/home.dart
+++ lib/home.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-// class CurrencyConverter extends StatefulWidget {
-//   const CurrencyConverter({super.key});
-
-//   @override
-//   State<CurrencyConverter> createState() => _CurrencyConverterState();
-// }
-
-// class _CurrencyConverterState extends State<CurrencyConverter> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Placeholder();
-//   }
-// }
-
 class Home extends StatefulWidget {
   const Home({super.key});
 
